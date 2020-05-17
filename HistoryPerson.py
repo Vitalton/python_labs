beb6745cb49f4ef64a0be2a669b2e317f06ce1ff
Vitaliy <vitalton76@gmail.com>
2020-05-17 12:40:55 +0300

    edit lab12

--- HistoryPerson.py
+++ HistoryPerson.py
@@ -1,19 +1,16 @@
 import random
 class HistoryPerson():
-    '''АТРИБУТ  "Только для чтения"'''
-    __character = "Настойчивость"
-
     '''СПЕЦИАЛЬНЫЕ МЕТОДЫ'''
-    def __init__(self, name, industry):
-        self.name = name
+    def __init__(self, surname, industry):
+        self.__surname = surname # АТРИБУТ "Только для чтения"
         self.industry = industry
         self.year_birth = random.randint(1580, 1630)
         self.year_death = random.randint(1630, 1680)
-        if (self.year_birth - self.year_death) > 30:
-            self.year_birth -= 15
-            self.year_death += 15
+        if (self.year_death - self.year_birth) > 24:
+            self.year_birth -= 10
+            self.year_death += 10
     def __str__(self):
-        return self.name + " - это историческая личность, которая внесла большой вклад в развитие науки"
+        return self.__surname + " - это историческая личность, которая внесла большой вклад в развитие науки"
     def __del__(self):
         print("Экземпляр удалён!")
 
@@ -21,7 +18,7 @@ class HistoryPerson():
     @staticmethod
     def status(self):
         print("Инфо о исторической личности:")
-        print("Имя:", self.name)
+        print("Фамилия:", self.__surname)
 
     '''ЗАКРЫТЫЙ МЕТОД'''
     def __privateMethod(self):
--- lab12.py
+++ lab12.py
@@ -1,7 +1,29 @@
 from HistoryPerson import HistoryPerson
-class A(HistoryPerson):
-    def __init__(self, surname):
-        self.surname  = surname
-a = A("issak")
-a.name = "newton"
-print(a.surname + "  " + a.name)
+
+class Add_info(HistoryPerson):
+    def __init__(self, surname, name, industry):
+        super().__init__(surname, industry)
+        self.name  = name
+
+    def setYear(self, year):
+        if year >= 1580 and year <= 1630:
+            self.year_birth = year
+        if year >= 1630 and year <= 1680:
+            self.year_death = year
+        else:
+            print("Введено некорректное значение!")
+    def age(self):
+        return self.year_death - self.year_birth
+    def getInfo(self):
+        print("Фамилия: ", self._HistoryPerson__surname, "\n" + "Имя: ", self.name, "\n" + "Отрасль: ",
+              self.industry, "\n" + "Год рождения: ",
+              self.year_birth, "\n" + "Год смерти: ",
+              self.year_death, "\n" + "Возраст: ",
+              self.age(), "\n")
+
+
+a = Add_info("newton", "issak", "phisic")
+a.getInfo()
+a.setYear(1674)
+a.getInfo()
+
